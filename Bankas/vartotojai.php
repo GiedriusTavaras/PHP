c866a8208d646024b094a4b7856b228240a0aa5b
GiedriusTavaras <67259761+GiedriusTavaras@users.noreply.github.com>
2020-10-14 12:54:27 +0300

    Delete vartotojai.php

--- Bankas/vartotojai.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$vartotojai = [
-    ['password' => md5('123'), 'vardas' => 'PHP'],
-    ['password' => md5('867482605'), 'vardas' => 'Giedrius'],
-];
-
-
-file_put_contents('slaptazodziai.json', json_encode($vartotojai));
