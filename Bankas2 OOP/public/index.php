97123a4b4e0cd4061f93e1b93cbcba716d669e6e
GiedriusTavaras <67259761+GiedriusTavaras@users.noreply.github.com>
2020-10-21 16:02:10 +0300

    Add files via upload

---
+++ Bankas2 OOP/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+define('DIR', str_replace('\\public', '', __DIR__));
+require DIR.'/vendor/autoload.php';
+require DIR.'/bootstrap/bootstrap.php';
+require DIR.'/route/route.php';
+
+// $route = str_replace($path, '', $_SERVER['REQUEST_URI']);
+
+
+
+
